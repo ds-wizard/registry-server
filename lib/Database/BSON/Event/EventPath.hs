f09c7f4c7403e439febc708a056db753429611ee
Vojtech Knaisl <vknaisl@gmail.com>
2019-07-18 11:36:15 +0200

    Convert BSON serialization from functions to typeclass instances

--- lib/Database/BSON/Event/EventPath.hs
+++ lib/Database/BSON/Event/EventPath.hs
@@ -4,15 +4,15 @@ import Control.Lens ((^.))
 import qualified Data.Bson as BSON
 import Data.Bson.Generic
 
-import Database.BSON.Common
+import Database.BSON.Common ()
 import LensesConfig
 import Model.Event.EventPath
 
 instance ToBSON EventPathItem where
-  toBSON value = ["type" BSON.=: value ^. pType, "uuid" BSON.=: serializeUUID (value ^. uuid)]
+  toBSON value = ["type" BSON.=: value ^. pType, "uuid" BSON.=: (value ^. uuid)]
 
 instance FromBSON EventPathItem where
   fromBSON doc = do
     epType <- BSON.lookup "type" doc
-    epUuid <- deserializeMaybeUUID $ BSON.lookup "uuid" doc
+    epUuid <- BSON.lookup "uuid" doc
     return EventPathItem {_eventPathItemPType = epType, _eventPathItemUuid = epUuid}
